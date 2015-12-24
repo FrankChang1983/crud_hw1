3dae4ee613aaf5b036b76405f35670193bc6cf99
SHOUTSE <frankchang1983@gmail.com>
2015-12-25 03:53:48 +0800

    g user model,and category,and settings

--- demo/db/migrate/20151224151944_create_topics.rb
+++ demo/db/migrate/20151224151944_create_topics.rb
@@ -1,7 +1,9 @@
 class CreateTopics < ActiveRecord::Migration
   def change
     create_table :topics do |t|
-
+      t.string :name
+      t.text :content
+      t.integer :category_id
       t.timestamps null: false
     end
   end
